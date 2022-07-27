0c397ec2394f1dba2cfa134b4665717a4a53ef9f
Avbarkova <avbarkova@gmail.com>
2022-07-27 21:53:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    while (expr.length%10!==0) {
+        expr = '0'+expr;
+    }
+    let arr = [];
+    for (let i=0; i<expr.length; i++) {
+        arr.push(expr.slice(i, i+10));
+        i=i+9;
+    }
+    let word = '';
+    for (let j=0; j<arr.length; j++) {
+        if (arr[j] !== '**********') {
+            let letter = '';
+            for (let n=0; n<arr[j].length; n++) {
+                if (arr[j].slice(n, n+2) == '10') {
+                    letter=letter+'.';
+                }
+                if (arr[j].slice(n, n+2) == '11') {
+                    letter=letter+'-';
+                }
+                n=n+1;
+            } 
+            for (key in MORSE_TABLE) {
+                if (key==letter) {
+                    word = word+MORSE_TABLE[key];
+                }; 
+            }
+        } else {
+            word = word+' ';
+        }
+    }
+return word;
 }
 
 module.exports = {
